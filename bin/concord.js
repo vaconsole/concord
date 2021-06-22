06566615a58e97776bf3710b42e155c5a8338fd3
Marvin Wu <marvin.wu@urbanspring.hk>
2021-06-22 21:50:18 +0800

    t

--- bin/concord.js
+++ bin/concord.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 const program = require('commander')
 const Database = require('sqlite-async')
+const { init, populate, match } = require('../lib/index')
 
-program.command('init <dbPath> <table>').action(options => {
-console.log('hello world') 
+program.command('init <dbPath>').action(async (dbPath) => {
+  const db = await Database.open(dbPath)
+  await init(db)
 })
 
-
-program.command('search <dbPath> <table>').action(options => {
-console.log('hello world') 
-})
-
-
-
+program
+  .command('populate <dbPath> <table> <pk>')
+  .action(async (dbPath, table, pk) => {
+    const db = await Database.open(dbPath)
+    await populate(db, table, pk)
+  })
 
 program.parse(process.argv)
--- test/index.test.js
+++ test/index.test.js
@@ -6,19 +6,25 @@ const dbPath = 'test/input/input.db'
 let db = null
 beforeEach(async () => {
   const initSql = fs.readFileSync('test/input/init.sql', 'utf-8')
-  db = await Database.open(':memory:')
-  await db.exec(initSql)
+  try {
+    fs.unlinkSync(dbPath)
+  } catch (error) {}
+  db = await Database.open(dbPath)
 })
 
+afterEach(async () => {})
+
 test('basic_init', async () => {
+  await db.exec(fs.readFileSync('test/input/init.sql', 'utf-8'))
   const result = await concord.init(db)
   await concord.init(db)
 })
 
 test('basic_populate', async () => {
+  await db.exec(fs.readFileSync('test/input/init.sql', 'utf-8'))
   await concord.init(db)
   const output = await concord.populate(db, 'a', 'id')
-  const result = await db.all('select * from con_match')
+  const result = await db.all('select * from concordance_id')
   expect(result).toEqual([
     { source: 'a', source_id: 'a1', con_id: 'a1' },
     { source: 'a', source_id: 'a2', con_id: 'a2' },
@@ -27,10 +33,11 @@ test('basic_populate', async () => {
 })
 
 test('basic_populate 2', async () => {
+  await db.exec(fs.readFileSync('test/input/init.sql', 'utf-8'))
   await concord.init(db)
   await concord.populate(db, 'a', 'id')
   await concord.populate(db, 'a', 'id')
-  const result = await db.all('select * from con_match')
+  const result = await db.all('select * from concordance_id')
   expect(result).toEqual([
     { source: 'a', source_id: 'a1', con_id: 'a1' },
     { source: 'a', source_id: 'a2', con_id: 'a2' },
@@ -39,10 +46,42 @@ test('basic_populate 2', async () => {
 })
 
 test('basic_match', async () => {
+  await db.exec(fs.readFileSync('test/input/init_1.sql', 'utf-8'))
   await concord.init(db)
   await concord.populate(db, 'a', 'id')
+  await concord.populate(db, 'b', 'id')
   await concord.populate(db, 'c', 'id')
-  await concord.match(db, 'c')
-  // const result = await db.all('select * from con_match')
-  // console.log(result)
+  await concord.match(db, 'a', ['id', 'ref'])
+  const result = await db.all('select * from concordance_id')
+  expect(result).toEqual([
+    { source: 'a', source_id: 'a1', con_id: 'a1' },
+    { source: 'a', source_id: 'a2', con_id: 'a2' },
+    { source: 'a', source_id: 'a3', con_id: 'a3' },
+    { source: 'b', source_id: 'b3', con_id: 'b3' },
+    { source: 'c', source_id: 'c2', con_id: 'c2' },
+    { source: 'b', source_id: 'b1', con_id: 'a1' },
+    { source: 'b', source_id: 'b2', con_id: 'a2' },
+    { source: 'c', source_id: 'c1', con_id: 'a3' },
+  ])
+})
+
+test('basic_match_2', async () => {
+  await db.exec(fs.readFileSync('test/input/init_2.sql', 'utf-8'))
+  await concord.init(db)
+  await concord.populate(db, 'a', 'id')
+  await concord.populate(db, 'b', 'id')
+  await concord.populate(db, 'c', 'id')
+  await concord.match(db, 'a', ['id', 'ref'])
+  const result = await db.all('select * from concordance_id')
+  expect(result).toEqual([
+    { source: 'a', source_id: 'a1', con_id: 'a1' },
+    { source: 'a', source_id: 'a2', con_id: 'a2' },
+    { source: 'a', source_id: 'a3', con_id: 'a3' },
+    { source: 'b', source_id: 'b2', con_id: 'b2' },
+    { source: 'b', source_id: 'b3', con_id: 'b3' },
+    { source: 'c', source_id: 'c2', con_id: 'c2' },
+    { source: 'b', source_id: 'b1', con_id: 'a1' },
+    { source: 'b', source_id: 'b1', con_id: 'a2' },
+    { source: 'c', source_id: 'c1', con_id: 'a3' },
+  ])
 })
