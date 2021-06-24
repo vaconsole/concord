f4d4c8425be3d75853c2e40b88122026681e4aa7
Marvin Wu <marvin.wu@urbanspring.hk>
2021-06-24 14:10:36 +0800

    fixed cli

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ## CI Status
 
 ![test](https://github.com/vaconsole/concord/actions/workflows/test.yml/badge.svg)
+
+
+## How to use
+
+➜  concord git:(master) node bin/concord.js --help
+Usage: concord [options] [command]
+
+Options:
+  -h, --help                             display help for command
+
+Commands:
+  init <dbPath>
+  populate <dbPath> <table> <pk>
+  match <dbPath> <table> <indexColumns>
+  help [command]                         display help for command
--- bin/concord.js
+++ bin/concord.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 const program = require('commander')
-const Database = require('sqlite-async')
+const Database = require('better-sqlite3')
 const { init, populate, match } = require('../lib/index')
 
-program.command('init <dbPath>').action(async (dbPath) => {
-  const db = await Database.open(dbPath)
-  await init(db)
+program.command('init <dbPath>').action((dbPath) => {
+  const db = new Database(dbPath)
+  init(db)
 })
 
 program
   .command('populate <dbPath> <table> <pk>')
-  .action(async (dbPath, table, pk) => {
-    const db = await Database.open(dbPath)
-    await populate(db, table, pk)
+  .action((dbPath, table, pk) => {
+    const db = new Database(dbPath)
+    populate(db, table, pk)
   })
 
 program
   .command('match <dbPath> <table> <indexColumns>')
-  .action(async (dbPath, table, indexColumns) => {
-    const db = await Database.open(dbPath)
-    await match(db, table, indexColumns.split(','))
+  .action((dbPath, table, indexColumns) => {
+    const db = new Database(dbPath)
+    match(db, table, indexColumns.split(','))
   })
 
 program.parse(process.argv)
