f299b49a3dc1354e58a804287e1c3ce3be14a9a5
Marvin Wu <marvin.wu@urbanspring.hk>
2021-06-28 17:49:54 +0800

    insertJsonToTable

--- test/job.test.js
+++ test/job.test.js
@@ -37,6 +37,49 @@ test('jsonCreateTable', () => {
   expect(result).toEqual([{ a: 'test', b: 1 }])
 })
 
+test('insertJsonToTable basic', () => {
+  const data = [
+    {
+      a: 'test',
+    },
+    {
+      b: 1,
+    },
+  ]
+  const SQL = concord.jsonCreateTable('job_1', data)
+  db.exec(SQL)
+  concord.insertJsonToTable(db, 'job_1', data)
+  // db.exec(SQL)
+  // db.exec(`insert into job_1 (a,b) values ('test',1)`)
+  const result = db.prepare('select * from job_1').all()
+  expect(result).toEqual([
+    { a: 'test', b: null },
+    { a: null, b: 1 },
+  ])
+})
+
+test('insertJsonToTable multiple', () => {
+  const data = [
+    {
+      a: 'test',
+      b: 2,
+    },
+    {
+      b: 1,
+    },
+  ]
+  const SQL = concord.jsonCreateTable('job_1', data)
+  db.exec(SQL)
+  concord.insertJsonToTable(db, 'job_1', data)
+  // db.exec(SQL)
+  // db.exec(`insert into job_1 (a,b) values ('test',1)`)
+  const result = db.prepare('select * from job_1').all()
+  expect(result).toEqual([
+    { a: 'test', b: 2 },
+    { a: null, b: 1 },
+  ])
+})
+
 test('createJob', () => {
   db.exec(fs.readFileSync('test/input/init_2.sql', 'utf-8'))
   concord.initJob(db)
@@ -46,4 +89,21 @@ test('createJob', () => {
     sql: 'select * from a',
   }
   concord.createJob(db, job)
+})
+
+test('submitJob', () => {
+  db.exec(fs.readFileSync('test/input/init_2.sql', 'utf-8'))
+  concord.initJob(db)
+  const job = {
+    description: 'test job',
+    action: 'replace',
+    sql: 'select * from a',
+  }
+  concord.createJob(db, job)
+  const data = [
+    { rowid: 1, id: 'a1', ref: 'b1' },
+    { rowid: 2, id: 'a2', ref: 'b1' },
+    { rowid: 3, id: 'a3', ref: 'c1' },
+  ]
+  concord.submitJob(db, 1, data)
 })
